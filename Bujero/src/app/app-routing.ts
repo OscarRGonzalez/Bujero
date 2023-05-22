a60f5ece896dbaee9ce602f20440c1529769f082
Oscar Ramon Gonzalez Guerra <oscargg107@gmail.com>
2023-05-22 19:51:27 +0200

    Home terminado

--- Bujero/src/app/app-routing.ts
+++ Bujero/src/app/app-routing.ts
@@ -2,10 +2,13 @@ import { RouterModule } from '@angular/router';
 
 //aquí se definen las rutas de cada componente
 import { HomeComponent } from './components/home/home.component';
+import { InfoPlantaComponent } from './components/info-planta/info-planta.component';
 
 
 const appRoutes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'planta', component: InfoPlantaComponent },
+
 ];
 
 
