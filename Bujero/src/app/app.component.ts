5e3b08767e13863624f19f597f2d99daf803da06
Jolie Alain <joliem27@gmail.com>
2023-06-01 20:50:59 +0200

    Interfaz de busqueda
    Creación de interfaz de la pestaña de búsqueda.
    Cambio de detalles en mis cultivos.
    Implementacion de navegación entre pestañas y se cambia el nombre del titulo por pestaña y el color del icono.

--- Bujero/src/app/app.component.ts
+++ Bujero/src/app/app.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bujero';
+  title_pagina: any = "Home";
+  select_cultivos: Boolean = false;
+  select_home: Boolean = true;
+  select_fav: Boolean = false;
+  select_busqueda: Boolean = false;
+
+  toHome() {
+    this.title_pagina = "Home";
+    if(this.select_home == false){
+      this.select_home = true;
+    }
+    this.select_fav = false;
+    this.select_cultivos = false;
+    this.select_busqueda = false;
+  }
+
+  toSearch() {
+    this.title_pagina = "Búsqueda";
+    if(this.select_busqueda == false){
+      this.select_busqueda = true;
+    }
+    this.select_fav = false;
+    this.select_cultivos = false;
+    this.select_home = false;
+  }
+
+
+  toFavs() {
+    this.title_pagina = "Favoritos";
+    if(this.select_fav == false){
+      this.select_fav = true;
+    }
+    this.select_busqueda = false;
+    this.select_cultivos = false;
+    this.select_home = false;
+  }
+
+  toMisCultivos() {
+    if(this.select_cultivos == false){
+      this.select_cultivos = true;
+    }
+    this.select_busqueda = false;
+    this.select_fav = false;
+    this.select_home = false;
+
+    this.title_pagina = "Mis cultivos";
+  }
+
 }
