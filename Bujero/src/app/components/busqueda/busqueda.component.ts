c2b81a491642f08edb6b81f0d0d89a3e1aeaa493
miguelwang125 <60920243+miguelwang125@users.noreply.github.com>
2023-06-04 23:55:09 +0200

    Buscador

--- Bujero/src/app/components/busqueda/busqueda.component.ts
+++ Bujero/src/app/components/busqueda/busqueda.component.ts
@@ -5,6 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
+
+
 export class BusquedaComponent {
+
+
+  plantas = [
+    {
+      nombre: 'Coleo',
+      path: 'assets/img/Coleo.png'
+    },{
+      nombre: 'Primula',
+      path: 'assets/img/primula.jpg'
+    },{
+      nombre: 'Margarita',
+      path: 'assets/img/margaritas.jpg'
+    },{
+      nombre: 'Girasol',
+      path: 'assets/img/girasoles.png'
+    },{
+      nombre: 'Rosa',
+      path: 'assets/img/rosas.png'
+    },{
+      nombre: 'Hortensia',
+      path: 'assets/img/hortensia.png' 
+    },{
+      nombre: 'Tulipanes',
+      path: 'assets/img/tulipanes.webp'
+    }
+  ];
+
+  searchTerm: string = '';
+  plantasFiltradas: any[] = [];
+
+  filtrarPlantas() {
+    this.plantasFiltradas = this.plantas.filter(planta =>
+      planta.nombre.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
 
 }
