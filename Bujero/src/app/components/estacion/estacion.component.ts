f77e91c8471fa5a2f749ac82461f3fc984424deb
miguelwang125 <60920243+miguelwang125@users.noreply.github.com>
2023-06-04 19:50:55 +0200

    Estacion

--- Bujero/src/app/components/estacion/estacion.component.ts
+++ Bujero/src/app/components/estacion/estacion.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./estacion.component.css']
 })
 export class EstacionComponent {
+  
+
+  toggleStar(planta: any) {
+    planta.star = !planta.star;
+  }
+
+  toggleMisCultivos(planta: any) {
+    planta.misCultivos = !planta.misCultivos;
+  }
+
+
+  plantas = [
+    {
+      nombre: 'Coleo',
+      path: 'assets/img/Coleo.png', 
+      misCultivos: true,
+      star: false
+    },{
+      nombre: 'Primula',
+      path: 'assets/img/primula.jpg',
+      misCultivos: false,
+      star: true
+    },{
+      nombre: 'Margarita',
+      path: 'assets/img/margaritas.jpg',
+      misCultivos: false,
+      star: true
+    },{
+      nombre: 'Girasol',
+      path: 'assets/img/girasoles.png',
+      misCultivos: false,
+      star: false
+    },{
+      nombre: 'Rosa',
+      path: 'assets/img/rosas.png',
+      misCultivos: true,
+      star: true
+    },{
+      nombre: 'Hortensia',
+      path: 'assets/img/hortensia.png', 
+      misCultivos: false,
+      star: true
+    },{
+      nombre: 'Tulipanes',
+      path: 'assets/img/tulipanes.webp', 
+      misCultivos: true,
+      star: true
+    }
+  ];
 
 }
